ee5e0625bdea7666b85105ecf0a252555b795273
Dennis Merkus <dennis.merkus@pm.me>
2020-08-26 11:02:40 +0200

    Add a Document constructor

--- src/documents/Document.ts
+++ src/documents/Document.ts
@@ -1,7 +1,27 @@
 import { Parent } from '../base'
+import { Token } from '../tokens'
+
+export interface DocumentData {
+  id: string
+  type: string
+
+  wordCount: number
+  children: (DocumentData | TextDocumentData)[]
+
+  lexemeIds: string[]
+  wordIds: string[]
+}
+
+export interface TextDocumentData extends DocumentData {
+  type: 'Text'
+
+  tokens: Token[]
+}
 
 export default class Document extends Parent {
-  constructor(type: string) {
-    super(type)
+  constructor(data: DocumentData) {
+    super(data.type)
+
+    this.children.push(...data.children.map((child) => new Document(child)))
   }
 }
